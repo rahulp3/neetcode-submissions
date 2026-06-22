2446eb5620e4b45e7d281d27be477a3e18aff73f
Rahul P B <60131823+rahulp3@users.noreply.github.com>
2026-06-22 20:40:45 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+
+
+    public int countOnes(int num){
+        int count = 0;
+        
+        while(num != 0){
+            int res = num & 1;
+            if(res == 1){
+                count++;
+            }
+            num = num >>> 1;
+        }
+        return count;
+    }
+
+    public int[] countBits(int n) {
+        int[] res = new int[n+1];
+        for(int i = 0 ; i <= n ; i++){
+            res[i] = countOnes(i);
+        }
+
+        return res;
+    }
+}
